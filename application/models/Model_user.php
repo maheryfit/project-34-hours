c0a257c244204d41bfee03f189faba3b3b3bb19f
hasinjara <wenzelhasinjara@gmail.com>
2023-02-07 17:37:51 +0300

    Update Model_user.php

--- application/models/Model_user.php
+++ application/models/Model_user.php
@@ -5,15 +5,15 @@ class Model_user extends CI_Model
 {
     
     public function verify_Login($nom,$mail,$mdp) {
-        $tab = array();
+        $val = 'not_found';
         $request = "SELECT * from utilisateur where (nom = '%s' or mail = '%s') and motdepasse = '%s'";
         $request = sprintf($request,$this->db->escape($nom), $this->db->escape($mail), $this->db->escape($mdp));
         $query = $this->db->query($request);
         $row = $query->row();
         if(isset($row)) {
-            return true;
+            $val = $row->idutilisateur;
         }
-    return false;
+    return $val;
     }
 
     public function inscription($nom, $mail, $mdp) {
