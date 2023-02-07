eb5263d5c4e205c993dfb3918c866b0b5a901a8f
maheryfit <mahery_fitahiana@yahoo.com>
2023-02-07 18:42:42 +0300

    propostion client page

---
+++ application/views/body/proposition-client.php
@@ -0,0 +1,124 @@
+<div class="container py-4 py-xl-5">
+        <div class="row mb-5">
+            <div class="col-md-8 col-xl-6 text-center mx-auto">
+                <h1>Proposition</h1>
+            </div>
+        </div>
+        <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3">
+            <div class="col">
+                <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                    <div class="card-body p-4">
+                        <p class="text-info card-text mb-0" id="title">Article</p>
+                        <p class="card-text">Mon <span>Nom de l'article</span> à échanger avec <span>Nom de l'article</span></p>
+                        <div class="d-flex justify-content-arround">
+                            <div class="col-6">
+                                <p class="fw-bold mb-0">John Smith</p>
+                                <p class="text-muted mb-0">Erat netus</p>
+                            </div>
+                            <div class="col-6">
+                                <a href="#" class="btn" id="discard">Annuler</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                    <div class="card-body p-4">
+                        <p class="text-info card-text mb-0" id="title">Article</p>
+                        <p class="card-text">Mon <span>Nom de l'article</span> à échanger avec <span>Nom de l'article</span></p>
+                        <div class="d-flex justify-content-arround">
+                            <div class="col-6">
+                                <p class="fw-bold mb-0">John Smith</p>
+                                <p class="text-muted mb-0">Erat netus</p>
+                            </div>
+                            <div class="col-6">
+                                <a href="#" class="btn" id="discard">Annuler</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                    <div class="card-body p-4">
+                        <p class="text-info card-text mb-0" id="title">Article</p>
+                        <p class="card-text">Mon <span>Nom de l'article</span> à échanger avec <span>Nom de l'article</span></p>
+                        <div class="d-flex justify-content-arround">
+                            <div class="col-6">
+                                <p class="fw-bold mb-0">John Smith</p>
+                                <p class="text-muted mb-0">Erat netus</p>
+                            </div>
+                            <div class="col-6">
+                                <a href="#" class="btn" id="discard">Annuler</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+<div class="container py-4 py-xl-5">
+        <div class="row mb-5">
+            <div class="col-md-8 col-xl-6 text-center mx-auto">
+                <h1>Proposition de l'extérieur</h1>
+            </div>
+        </div>
+        <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3">
+            <div class="col">
+                <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                    <div class="card-body p-4">
+                        <p class="text-info card-text mb-0" id="title">Article</p>
+                        <p class="card-text">Mon <span>Nom de l'article</span> à échanger avec <span>Nom de l'article</span></p>
+                        <div class="d-flex justify-content-arround">
+                            <div class="col-6">
+                                <p class="fw-bold mb-0">John Smith</p>
+                                <p class="text-muted mb-0">Erat netus</p>
+                            </div>
+                            <div class="col-6">
+                                <a href="#" class="btn btn-link">Accepter</a>
+                                <a href="#" class="btn" id="discard">Rejeter</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                    <div class="card-body p-4">
+                        <p class="text-info card-text mb-0" id="title">Article</p>
+                        <p class="card-text">Mon <span>Nom de l'article</span> à échanger avec <span>Nom de l'article</span></p>
+                        <div class="d-flex justify-content-arround">
+                            <div class="col-6">
+                                <p class="fw-bold mb-0">John Smith</p>
+                                <p class="text-muted mb-0">Erat netus</p>
+                            </div>
+                            <div class="col-6">
+                                <a href="#" class="btn btn-link">Accepter</a>
+                                <a href="#" class="btn" id="discard">Rejeter</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                    <div class="card-body p-4">
+                        <p class="text-info card-text mb-0" id="title">Article</p>
+                        <p class="card-text">Mon <span>Nom de l'article</span> à échanger avec <span>Nom de l'article</span></p>
+                        <div class="d-flex justify-content-arround">
+                            <div class="col-6">
+                                <p class="fw-bold mb-0">John Smith</p>
+                                <p class="text-muted mb-0">Erat netus</p>
+                            </div>
+                            <div class="col-6">
+                                <a href="#" class="btn btn-link">Accepter</a>
+                                <a href="#" class="btn" id="discard">Rejeter</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
