1e7667fc200832c7007223c0548c979aef0dbb4a
maheryfit <mahery_fitahiana@yahoo.com>
2023-02-08 10:01:22 +0300

    changement

--- application/views/body/accueil-admin.php
+++ application/views/body/accueil-admin.php
@@ -5,7 +5,7 @@
                 <div class="col">
                     <?php
                         foreach ($listecategories as $listecategorie) { ?>
-                            <h3 class="mb-2"><?= $listecategorie["nom"];?></h3>
+                            <h3 class="mb-2"><?= ucfirst($listecategorie["nom"]);?></h3>
                         <?php }
                     ?>
                 </div>
@@ -14,7 +14,7 @@
     </div>
     <form method="post" action="<?= site_url("controlleur_user/traitement_insertion_categorie");?>" class="mb-3 mx-auto col-6">
         <label for="formFileMultiple" class="form-label">Ajouter une nouvelle catégorie :</label>
-        <input name="categorie" class="form-control" type="text" id="formFileMultiple">
+        <input name="nom" class="form-control" type="text" id="formFileMultiple">
         <div class="mb-3"><button class="btn d-block w-100" type="submit">Ajouter</button></div>
     </form>
 </section>
